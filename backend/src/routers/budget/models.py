ceca85f338bc320d676fe9f376b3432723f1682c
ZhuchkaTriplesix <mrlololoshka94@gmail.com>
2025-07-24 09:02:48 +0300

    models for budget

---
+++ backend/src/routers/budget/models.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Integer, ForeignKey, Numeric, DateTime
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from backend.src.database.core import Base
+from datetime import datetime
+
+class Budget(Base):
+    __tablename__ = "budgets"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    user_id: Mapped[int] = mapped_column(Integer, ForeignKey("users.id"))
+    category_id: Mapped[int] = mapped_column(Integer, ForeignKey("categories.id"))
+    amount_limit: Mapped[float] = mapped_column(Numeric(12, 2), nullable=False)
+    period_start: Mapped[datetime] = mapped_column(DateTime, nullable=False)
+    period_end: Mapped[datetime] = mapped_column(DateTime, nullable=False)
+    user = relationship("User", back_populates="budgets")
+    category = relationship("Category", back_populates="budgets") 
